ea4fc69cdaaf27a17904b3cf509c950c9544dbd9
adrocodes <adrocodes@users.noreply.github.com>
2023-05-28 14:02:11 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# CBR Cafes
+
+## Setup
+
+1. Make sure you have Docker installed.
+2. Run `docker compose up -d` to start the service
+3. Update your hosts files with: `127.0.0.1 cbr-cafes.local`
+4. Visit http://cbr-cafes.local:8080
---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Docker Test</title>
+
+  <style>
+    *,
+    *::before,
+    *::after {
+      box-sizing: border-box;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php
+  $heading = "PHP Docker Test";
+  ?>
+
+  <h1><?php echo $heading; ?></h1>
+
+  <img src="/assets/test.jpg" alt="test image" style="max-width: 100%; display: block;" />
+
+</body>
+
+</html>
