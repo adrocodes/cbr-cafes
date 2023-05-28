2cef1a22251a5c480ff49613abcfa860a0250a52
adrocodes <adrocodes@users.noreply.github.com>
2023-05-28 14:38:16 +1000

    configure composer

--- public/index.php
+++ public/index.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PHP Docker Test</title>
+require __DIR__ . '/vendor/autoload.php';
 
-  <style>
-    *,
-    *::before,
-    *::after {
-      box-sizing: border-box;
-    }
-  </style>
-</head>
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-<body>
-
-  <?php
-  $heading = "PHP Docker Test";
-  ?>
-
-  <h1><?php echo $heading; ?></h1>
-
-  <img src="/assets/test.jpg" alt="test image" style="max-width: 100%; display: block;" />
-
-</body>
-
-</html>
+echo $_ENV["DB"];
